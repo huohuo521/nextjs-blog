ec9de0af9cdeee7ba549201a96dd84167b2cef52
WB01745229 <lmy01745229@alibaba-inc.com>
2023-09-27 16:49:45 +0800

    feat: liumingyan

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = '[Your Name]';
+const name = '[Your Name: 刘明焱]';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
